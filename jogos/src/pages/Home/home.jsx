66d464caf3084b26e9e72e72d50502c36d50bc0f
paolarr <paola.guevara@aluno.senai.br>
2026-04-06 14:14:55 -0300

    home

---
+++ jogos/src/pages/Home/home.jsx
@@ -0,0 +1,35 @@
+import home from './home.module.css';
+import contato from '../Contato/contato';
+import produtos from '../Produtos/produto'
+function Home() {
+    return (
+        <div className={home.container}>
+            <header className={home.header}>
+                <h1>SURICATEAM</h1>
+            </header>
+
+            <main className={home.main}>
+                <p className={home.subtitulo}>
+                    Bem-vindo à SURICATEAM! Seu site de jogos FPS favorito.
+                </p>
+                <hr className={home.linha} />
+                <p className={home.descricao}>
+                    Aqui você encontrará variedades de jogos FPS
+                </p>
+                
+                <div className={home.botao}>
+                    <button className={home.botaojogos}>JOGOS</button>
+                    <button className={home.botaocontato}>CONTATO</button>
+                </div>
+            </main>
+
+            <footer className={home.footer}>
+                <p className={home.rodape}>
+                    © 2024 SURICATEAM. Todos os direitos reservados.
+                </p>
+            </footer>
+        </div>
+    );
+}
+
+export default Home;
